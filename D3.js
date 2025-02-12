d57c49b70cdb473427798feeb2589b43020045f9
Roberto Marino <rm.marinoroberto@gmail.com>
2025-02-12 18:14:01 +0100

    last update

--- D3.js
+++ D3.js
@@ -187,11 +187,11 @@ console.log("La massa totale dell'equipaggio è:", crewMass);
 */
 if (crewMass < 500) {
   console.log("Ship is under loaded");
-} else if ((crewMass >= 500, crewMass < 700)) {
+} else if (crewMass >= 500 && crewMass < 700) {
   console.log("Ship is half loaded");
-} else if ((crewMass >= 700, crewMass < 900)) {
+} else if (crewMass >= 700 && crewMass < 900) {
   console.log("Warning: Load is over 700");
-} else if ((crewMass >= 900, crewMass < 1000)) {
+} else if (crewMass >= 900 && crewMass < 1000) {
   console.log("Critical Load: Over 900");
 } else {
   console.log("DANGER! OVERLOAD ALERT: escape from ship now!");
@@ -209,11 +209,11 @@ console.log("Nuova massa totale dell'equipaggio", crewMass);
 
 if (crewMass < 500) {
   console.log("Ship is under loaded");
-} else if ((crewMass >= 500, crewMass < 700)) {
+} else if (crewMass >= 500 && crewMass < 700) {
   console.log("Ship is half loaded");
-} else if ((crewMass >= 700, crewMass < 900)) {
+} else if (crewMass >= 700 && crewMass < 900) {
   console.log("Warning: Load is over 700");
-} else if ((crewMass >= 900, crewMass < 1000)) {
+} else if (crewMass >= 900 && crewMass < 1000) {
   console.log("Critical Load: Over 900");
 } else {
   console.log("DANGER! OVERLOAD ALERT: escape from ship now!");
@@ -227,11 +227,3 @@ for (let i = 0; i < starWarsCharacters.length; i++) {
   }
 }
 console.log(starWarsCharacters);
-/* --EXTRA-- ESERCIZIO 9
-  Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "charactersNames" le stringhe corrispondenti a personaggi con lo stesso nome.
-  Una volta fatto crea un console.log per controllare la proprietà length di "charactersNames" prima e dopo l'operazione.
-*/
-
-/* --EXTRA-- ESERCIZIO 10
-  Crea una funzionalità che selezioni un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo (a tuo piacimento).
-*/
